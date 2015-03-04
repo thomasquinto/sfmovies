f91d7710e277f01fdb9edb6a9d1da5d964a5f9e3
Thomas Quinto <tquinto@viggle.com>
2015-03-03 23:34:42 -0800

    Add http server to app.js for 'forever' service to work.

--- app.js
+++ app.js
@@ -82,5 +82,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var server = app.listen(80, function() {
+    console.log('Listening on port %d', server.address().port);
+});
 
 module.exports = app;
